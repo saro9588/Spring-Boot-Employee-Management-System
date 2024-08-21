77fd455c4dd5c0081389fd7dad0ef5f0d4b050a6
Saro <babikiansaro@gmail.com>
2024-08-21 13:56:24 -0700

    create-updateemployee-rest-api

--- src/main/java/com/saro/dev/ems/service/EmployeeService.java
+++ src/main/java/com/saro/dev/ems/service/EmployeeService.java
@@ -1,11 +1,10 @@
 package com.saro.dev.ems.service;
-
 import com.saro.dev.ems.dto.EmployeeDto;
-
 import java.util.List;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
     EmployeeDto getEmployeeById(Long employeeId);
     List<EmployeeDto> getAllEmployees();
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
 }
